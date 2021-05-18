b5a8b87309da3a9afad1112cc8e984b0420c13b7
baitu520 <75007073+baitu520@users.noreply.github.com>
2021-05-18 17:30:35 +0800

    Update TouTiaoClient.php

--- core/Profile/TouTiaoClient.php
+++ core/Profile/TouTiaoClient.php
@@ -17,7 +17,7 @@ class TouTiaoClient
 {
     public static $access_token;
 
-    public static $server_url = 'https://ad.toutiao.com/open_api';
+    public static $server_url = 'https://ad.oceanengine.com/open_api';
 
     public static $box_url = 'https://test-ad.toutiao.com/open_api';
 
